5a30e8efc43e3061b5d61c44c931ca1500435f8d
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-03-13 22:36:01 -0300

    Save score

--- backend/dsmovie/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,13 +22,12 @@ public class Movie {
 	private Integer count;
 	private String image;
 	
-	
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
 	
 	public Movie() {
 	
 	}
-
-
 
 	public Movie(Long id, String title, Double score, Integer count, String image) {
 		super();
@@ -94,7 +97,8 @@ public class Movie {
 	public void setImage(String image) {
 		this.image = image;
 	}
-	
-	
 
+	public Set<Score> getScores() {
+		return scores;
+	}
 }
---
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/repositories/ScoreRepository.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.ScorePK;
+import com.devsuperior.dsmovie.entities.User;
+
+public interface ScoreRepository extends JpaRepository<Score, ScorePK>{
+	
+	//User findByEmail(String email);
+
+}
