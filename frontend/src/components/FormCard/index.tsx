5129bae77b85c6a49707937a73951fe36d2f844b
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-03-14 01:07:55 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,8 +1,9 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import React, { useEffect, useState } from "react";
 import {Link, useNavigate} from "react-router-dom"
 import { Movie } from "types/movie";
 import { BASE_URL } from "utils/requests";
+import { validateEmail } from "utils/validate";
 import './styles.css'
 
 type Props = {
@@ -10,6 +11,7 @@ type Props = {
 }
 function FormCard( {movieID} : Props) {
 
+    const navigate = useNavigate();
     const[movie, setMovie] = useState<Movie>();
 
     //const navigate = useNavigate();
@@ -23,12 +25,37 @@ function FormCard( {movieID} : Props) {
 
     // const handleSubmit = (event: React.FormEvent<HTMLFormElement>);
 
+    const handleSubmite = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const email = (event.target as any).email.value;
+        const score = (event.target as any).score.value;
+
+        if (!validateEmail(email)){
+            return; 
+        }
+
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: '/scores',
+            data: {
+                email: email,
+                movieId: movieID,
+                score: score
+            }
+        }
+
+        axios(config).then(response => {
+            navigate("/");
+        })
+    }
+
     return (
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title}/>
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie?.title}</h3>
-                <form className="dsmovie-form">
+                <form className="dsmovie-form" onSubmit={handleSubmite}>
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="email">Informe seu email</label>
                         <input type="email" className="form-control" id="email" />
